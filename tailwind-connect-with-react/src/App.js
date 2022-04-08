f5a44bf740f90a6347b85b369cd7c1d9394beefa
TouhinTechnical <touhidulislam1090@gmail.com>
2022-04-08 17:55:45 +0600

    create Pricing Card with React

--- tailwind-connect-with-react/src/App.js
+++ tailwind-connect-with-react/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
+import Pricing from './component/Pricing/pricing';
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
+        <Pricing></Pricing>
     </div>
   );
 }
